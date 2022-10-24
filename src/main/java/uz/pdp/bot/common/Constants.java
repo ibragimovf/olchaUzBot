e1a30aab2f16ca93a7e42112fc928962ad26a69b
Davlatbek <111009259+ibragimovf@users.noreply.github.com>
2022-10-24 17:23:51 +0500

    deletes

--- src/main/java/uz/pdp/bot/common/Constants.java
+++ src/main/java/uz/pdp/bot/common/Constants.java
@@ -1,6 +1,6 @@
 package uz.pdp.bot.common;
 
 public interface Constants {
-    String BOT_SECRET_KEY = "5307193060:AAGyB45-B32MffQw-bMnBtef0c6tJvvM3qA";
-    String BOT_USERNAME = "@smart_shop_online_bot";
+    String BOT_SECRET_KEY = "";
+    String BOT_USERNAME = "";
 }
